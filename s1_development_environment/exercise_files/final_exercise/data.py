68d3b85eb6b0b086e2fca9a3fe31af7e671aec0b
capsabogdan <capsabogdan@gmail.com>
2023-01-04 09:32:37 +0100

    .

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,8 +1,23 @@
 import torch
+import numpy as np
+import pdb
+from glob import glob
 
+# put the following statement where you want the code to stop and step to pdb
+# pdb.set_trace()
 
 def mnist():
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
+    from glob import glob
+    train = []
+    for file in glob("./corruptmnist/train*"):
+        crt = np.load(file)
+        train.append(crt)
+    print(train)
+    test = np.load("./corruptmnist/test.npz")
+    #train = torch.randn(50000, 784)
+    #test = torch.ra
+    # ndn(10000, 784) 
     return train, test
+
+mnist()
